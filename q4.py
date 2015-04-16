2eb6a3a4a3712ec302dd0e06bc68e7b93d4cfaea
OscarEduardo <Oscar_789456123@hotmail.com>
2015-04-16 12:02:12 -0500

    Update and rename q3.py to q4.py

---
+++ q4.py
@@ -0,0 +1,11 @@
+# Exam
+#Oscar Eduardo Sanchez
+def fibonacci(n):
+	if(n==0 or n==1):
+		return n
+	if(n>1):
+		return fibonacci(n-2)+fibonacci(n-1)
+
+x=int(input("Give me the position: "))
+r= fibonacci(x)
+print(r)
